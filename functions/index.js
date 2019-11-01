10b1f2aedc74895ac96142309cc2e46a18e4c0ed
Emmanuel Adeniyi <access2emma@gmail.com>
2019-11-01 14:18:54 +0100

    First commit. Added functionality to post to the server and add user id to the new created user information

---
+++ functions/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+
+// setting up firebase functions
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+const uuidv5 = require('uuid/v5');
+
+admin.initializeApp();
+
+const app = express();
+app.use(cors());
+
+app.post('/', async (request, response) => {
+    const userData = request.body || {};
+
+    const { firstName, lastName, birthday, age, hobby } = userData;
+
+    try {
+        const snapshot = await admin
+            .database()
+            .ref('/users')
+            .push({ firstName, lastName, birthday, age, hobby });
+        return response.status(200).json({
+            message: 'User information successfully saved',
+            data: userData
+        });
+    } catch (error) {
+        console.log(error);
+        return response.status(400).send('Unable to add the user');
+    }
+});
+
+exports.users = functions.https.onRequest(app);
+
+exports.updateUserID = functions.database.ref('/users/{userID}').onCreate((snapshot) => {
+    const randomUserID = uuidv5('https://enye.tech', uuidv5.URL); // generate random user id
+
+    // a promise most be return when performing asynchronous request like saving something in the real-time database
+    return snapshot.ref.child('userID').set(randomUserID);
+});
